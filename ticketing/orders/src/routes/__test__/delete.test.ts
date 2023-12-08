32dcce85d2b3cd1cc4d5089b97758630b76d3175
Slobodan Babic <babkeus@gmail.com>
2023-12-08 15:42:20 +0200

    Orders Service done

---
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+
+it('marks an order as cancelled', async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel an order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  const fetchedOrder = await Order.findById(order.id);
+  // make sure the order is cancelled
+  expect(fetchedOrder?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits an order cancel event');
